4873e4711bcfebc6189fc00b6b7318ba61b9916f
ADRIEL JHAMES DOS PASSOS SILVA <2024204240@unibrasil.com.br>
2025-11-03 19:25:14 -0300

    adicionado os aquivos php

---
+++ Main/api/login.php
@@ -0,0 +1,24 @@
+<?php
+    include_once('config.php');
+    $usuario = $_POST['email_login'];
+    $senha = $_POST['senha_login'];
+
+    $usuario = $conexao->real_escape_string($usuario);
+
+$result = mysqli_query($conexao,"SELECT senha_cadastro FROM cadastro WHERE email_cadastro = '$usuario'");
+
+if ($result->num_rows == 1) {
+  $row = $result->fetch_assoc();
+
+  if (password_verify($senha, $row['senha_cadastro'])) {
+    $_SESSION['usuario'] = $usuario;
+    
+  } else {
+    ;
+  }
+} else {
+  ;
+}
+
+$conexao->close();
+?>
